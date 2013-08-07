1edf9bb4a65e73ecd2f86e76e05fb65b88e5b39a
Plinio Balduino <pbalduino+github@gmail.com>
2013-08-07 11:20:12 -0300

    http and https are working

--- src/verbalexpressions/core.clj
+++ src/verbalexpressions/core.clj
@@ -1,6 +1,28 @@
 (ns verbalexpressions.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn start-of-line [] "^")
+
+(defn then [value]
+  (fn [] value))
+
+(defn maybe [value]
+  (fn [] (str "(" value ")?")))
+
+
+(defn anything-but [value])
+
+(defn end-of-line [])
+
+(defn- translate-to-regex [expr]
+  (loop [result ""
+         list expr]
+    (if (empty? list)
+        result
+        (recur (str result ((first list)))
+               (rest list)))))
+
+(defn verbal-expression [& expr]
+  (let [expression (translate-to-regex expr)
+        regex      (re-pattern expression)]
+    (println expression)
+    (fn [x] (not (empty? (re-seq regex x))))))
--- test/verbalexpressions/core_test.clj
+++ test/verbalexpressions/core_test.clj
@@ -3,5 +3,15 @@
             [verbalexpressions.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "A simple URL"
+    (let [valid? (verbal-expression start-of-line
+                                    (then "http")
+                                    (maybe "s"))
+;                                    (then "://")
+;                                    (maybe "www.")
+;                                    (anything-but " ")
+;                                    end-of-line)
+          url  "http://www.google.com"
+          urls "https://www.google.com"]
+    (is (valid? url))
+    (is (valid? urls)))))
